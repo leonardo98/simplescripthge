769c5dc5c42fe800c56e02a0d47a2fc53f22be0d
am98pln@gmail.com <am98pln@gmail.com@af30b2d4-25f6-c375-17c4-a89aa4467a3c>
2014-06-15 15:53:37 +0000

    - настройка проекта - новые вершинный шейдер, больше хаоса на синусах - переключение на вывод сетки

--- branches/water_ocean_shader/proekt/shader/WaterVS.glsl
+++ branches/water_ocean_shader/proekt/shader/WaterVS.glsl
@@ -19,10 +19,14 @@ void main()
  vertex = gl_Vertex;
  shadow_proj_coords = shadow_matrix * vertex;
 
- float h = 0.2 * sin(vertex.x * 0.1 + u_time)
-  + 0.25 * sin((vertex.x + vertex.z) * 0.5 + u_time * 0.8)
-  + 0.05 * sin(vertex.z * 5.5 + u_time * 3.0); 
-
+// float h = - 0.4 * abs(sin(vertex.x * 0.1 + u_time))
+//  - 0.5 * abs(sin((vertex.x + vertex.z) * 0.5 + u_time * 0.8))
+//  + 0.05 * sin(vertex.z * 5.5 + u_time * 3.0); 
+float f = sin(vertex.x * 0.4 + u_time) * sin((vertex.x + vertex.z) * 0.5 + u_time * 0.8);
+float h = - 0.1 * abs(f)
+  + 0.03 * sin(vertex.z * 5.5 + u_time * 3.0)
+  - 0.2 * abs(f * sin((vertex.x * vertex.x - vertex.z) * 1.6 + u_time * 1.9));
+ 
  vertex.y += h * 0.5;
 
  gl_TexCoord[0] = gl_MultiTexCoord0;
